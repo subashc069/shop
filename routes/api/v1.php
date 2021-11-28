5e3cc50abb1bf72f59ef271dcdeb5dbb45ac67c2
Subash Chaudhary <bcoolboy8@gmail.com>
2021-11-28 15:38:00 +0545

    added strict_types to all files

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,19 +1,21 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-})->name(name: 'auth:me');
+Route::middleware('auth:sanctum')
+    ->get('/user', function (Request $request) {
+        return $request->user();
+    })->name('auth:me');
 
 /**
  * Product Routes
  */
 Route::prefix('products')->as('products:')->group(function () {
     Route::get(
-        uri: '/',
-        action: App\Http\Controllers\Api\V1\Products\IndexController::class,
-    )->name(name: 'show');
+        '/',
+        App\Http\Controllers\Api\V1\Products\IndexController::class,
+    )->name('show');
 });
